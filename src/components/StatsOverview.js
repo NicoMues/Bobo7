bd128da3ab5832b178cc72e98255e4c3c0997c22
NicoMues <nicolas.mues@gmail.com>
2025-05-21 12:30:19 +0200

    jo

---
+++ src/components/StatsOverview.js
@@ -0,0 +1,13 @@
+// components/StatsOverview.js
+import React from 'react';
+
+function StatsOverview() {
+  return (
+    <div>
+      <h2>Statistiken</h2>
+      <p>Hier könnten Nutzungsstatistiken angezeigt werden.</p>
+    </div>
+  );
+}
+
+export default StatsOverview;
